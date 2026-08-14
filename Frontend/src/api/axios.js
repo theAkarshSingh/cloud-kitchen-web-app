e418f7ba8eedc3f6030f4c31d121bf3a8745a395
Akarsh Singh <theakarshsingh@gmail.com>
2026-08-14 06:36:46 +0530

    web app

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,15 +1,19 @@
 import axios from "axios";
+
 const api = axios.create({
-  baseURL: "/api/v1",
-  headers: { "Content-Type": "application/json" },
+  baseURL: import.meta.env.VITE_API_URL || "/api/v1",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-//attach a token.
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
@@ -20,6 +24,7 @@ api.interceptors.response.use(
       localStorage.removeItem("token");
       window.location.href = "/login";
     }
+
     return Promise.reject(err);
   },
 );
