e486d3aef8cacc81198da736b9d0bd56e5924f51
Akarsh Singh <theakarshsingh@gmail.com>
2026-08-14 06:54:21 +0530

    web app

--- Backend/index.js
+++ Backend/index.js
@@ -7,11 +7,6 @@ dns.setServers(["8.8.8.8", "1.1.1.1"]);
 import connectDB from "./config/db.config.js";
 import app from "./src/app.js";
 
-const PORT = process.env.PORT || 5000;
+await connectDB();
 
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
-    console.log(`API Docs: http://localhost:${PORT}/api-docs`);
-  });
-});
+export default app;
