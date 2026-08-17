4ee023d80df1f6fe91696e9cf30f6f8169bd3a1c
Akarsh Singh <theakarshsingh@gmail.com>
2026-08-17 18:57:17 +0530

    prod fixes

--- Backend/index.js
+++ Backend/index.js
@@ -7,6 +7,20 @@ dns.setServers(["8.8.8.8", "1.1.1.1"]);
 import connectDB from "./config/db.config.js";
 import app from "./src/app.js";
 
-await connectDB();
+const PORT = process.env.PORT || 3000;
+
+// Local development
+if (process.env.VERCEL !== "1") {
+    connectDB().then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server is running at http://localhost:${PORT}`);
+        });
+    });
+}
+
+// Vercel / production
+else {
+    await connectDB();
+}
 
 export default app;
